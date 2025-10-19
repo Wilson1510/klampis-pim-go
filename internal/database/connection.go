a5e4c48bd2bf2b9ad3cee1106b836b084ae41088
Wilson Soeparman <wilson.soeparman@gmail.com>
2025-10-19 16:04:32 +0800

    Add database connection implementation
    - Created a new file for establishing a PostgreSQL database connection using GORM.
    - Implemented the NewConnection function to handle connection setup and error checking.
    - Utilized configuration parameters for connection details, ensuring compatibility with the existing configuration structure.

---
+++ internal/database/connection.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+	"github.com/Wilson1510/klampis-pim-go/internal/config"
+)
+
+func NewConnection(config *config.DatabaseConfig) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Jakarta", 
+		config.Host, 
+		config.Port, 
+		config.User, 
+		config.Password, 
+		config.Name,
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	err = sqlDB.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
